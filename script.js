f52f87dfdb9c47b9148522cdd38e58b667013183
Jacob Park <jacobypark122@gmail.com>
2023-01-25 04:49:58 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,12 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "What a beautiful day!";
+
+document.querySelector("body").appendChild(h3)
+
+
+
