76accf90d22523dcf5b4c22e43db8dcc73004613
Jacob Park <jacobypark122@gmail.com>
2023-01-30 00:20:02 -0500

    Changing personal details + adding event listener

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('Thank you for reading!');
+});
+}
 
-const h3 = document.createElement("h3");
-h3.textContent = "What a beautiful day!";
-
-document.querySelector("body").appendChild(h3)
+addingEventListener()
 
 
 
